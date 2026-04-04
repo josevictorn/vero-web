60343386109259efe3eece5e9d7331843ea3ae4d
José Victor <josevictornascimento2016@gmail.com>
2026-04-04 14:02:35 -0300

    feat: :sparkles: restructure AppLayout to include SidebarInset and enhance layout with SidebarTrigger and Separator

--- src/routes/_app/layout.tsx
+++ src/routes/_app/layout.tsx
@@ -1,6 +1,11 @@
 import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 import { AppSidebar } from "@/common/components/sidebar";
-import { SidebarProvider } from "@/common/components/ui/sidebar";
+import { Separator } from "@/common/components/ui/separator";
+import {
+	SidebarInset,
+	SidebarProvider,
+	SidebarTrigger,
+} from "@/common/components/ui/sidebar";
 
 export const Route = createFileRoute("/_app")({
 	component: AppLayout,
@@ -22,7 +27,21 @@ function AppLayout() {
 	return (
 		<SidebarProvider>
 			<AppSidebar />
-			<Outlet />
+			<SidebarInset>
+				<header className="flex h-16 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+					<div className="flex gap-2 px-4">
+						<SidebarTrigger className="-ml-1" />
+						<Separator
+							className="mr-2 data-[orientation=vertical]:h-6"
+							orientation="vertical"
+						/>
+						<p className="font-medium text-sm tracking-tight">Dashboard</p>
+					</div>
+				</header>
+				<div className="flex flex-1 flex-col gap-4 p-4">
+					<Outlet />
+				</div>
+			</SidebarInset>
 		</SidebarProvider>
 	);
 }
