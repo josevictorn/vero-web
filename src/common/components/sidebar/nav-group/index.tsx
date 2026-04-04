e325cc44422ee3124d71e02b211f89e4bd175757
José Victor <josevictornascimento2016@gmail.com>
2026-04-04 13:10:17 -0300

    feat: :sparkles: add NavGroup component and associated types for sidebar navigation

---
+++ src/common/components/sidebar/nav-group/index.tsx
@@ -0,0 +1,33 @@
+import { Link, useLocation } from "@tanstack/react-router";
+import {
+	SidebarGroup,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+} from "@/common/components/ui/sidebar";
+import type { NavMainProps } from "./types";
+
+export function NavGroup({ items }: NavMainProps) {
+	const { pathname } = useLocation();
+
+	return (
+		<SidebarGroup className="group-data-[collapsible=icon]:hidden">
+			<SidebarMenu>
+				{items.map((item) => (
+					<SidebarMenuItem className="mx-1" key={item.title}>
+						<SidebarMenuButton asChild>
+							<Link
+								className="data-[current=true]:bg-sidebar-accent data-[current=true]:font-semibold"
+								data-current={pathname === item.url}
+								to={item.url}
+							>
+								<item.icon />
+								<span>{item.title}</span>
+							</Link>
+						</SidebarMenuButton>
+					</SidebarMenuItem>
+				))}
+			</SidebarMenu>
+		</SidebarGroup>
+	);
+}
