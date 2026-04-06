bc981860a9661cbfd474fc741b9d2a5f8f20182a
José Victor <josevictornascimento2016@gmail.com>
2026-04-05 22:58:08 -0300

    feat: :sparkles: add search schema for user pagination in ListUsers route

--- src/routes/_app/users/index.tsx
+++ src/routes/_app/users/index.tsx
@@ -1,9 +1,32 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { z } from "zod";
+import BaseContainer from "@/common/components/base-container";
+import { PageHeader } from "@/common/components/page-header";
+import { UsersList } from "@/modules/users/components/user-list";
+import { ListUsersController } from "@/modules/users/controllers/list-users-controller";
 
-export const Route = createFileRoute('/_app/users/')({
-  component: RouteComponent,
-})
+export const searchSchema = z.object({
+	page: z.coerce.number().default(1),
+});
+
+export const Route = createFileRoute("/_app/users/")({
+	component: RouteComponent,
+	validateSearch: searchSchema,
+});
 
 function RouteComponent() {
-  return <div>Hello "/_app/users/"!</div>
+	return (
+		<BaseContainer>
+			<PageHeader
+				buttonProps={{
+					text: "Adicionar usuário",
+				}}
+				subtitle="Gerencie os usuários da plataforma e suas permissões de acesso"
+				title="Gerecionamento de usuários"
+			/>
+			<ListUsersController>
+				{(props) => <UsersList {...props} />}
+			</ListUsersController>
+		</BaseContainer>
+	);
 }
